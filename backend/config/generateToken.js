8023fe150a6173820611fe556267d4f6243fefac
Giribabi <sammidisaroja@gmail.com>
2024-06-16 22:48:35 +0530

    secretized token

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const secret_token = process.env.JWT_SECRET_TOKEN || "DON'T_MESS_WITH_HIPPOS";
+const secret_token = process.env.JWT_SECRET_TOKEN;
 
 const generateToken = (id) => {
     //console.log(process.env.JWT_SECRET_TOKEN);
